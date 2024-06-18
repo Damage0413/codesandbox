1f890df670116d67e3dfdb915fcdf59a2a8ee71c
mzw <1838156587@qq.com>
2024-06-18 11:47:05 +0800

    核心功能代码

---
+++ src/main/java/com/oj/codesandbox/model/ExecuteMessage.java
@@ -0,0 +1,27 @@
+package com.oj.codesandbox.model;
+
+import lombok.Data;
+
+@Data
+public class ExecuteMessage {
+    /**
+     * 执行结果代码
+     */
+    private Integer value;
+
+    /**
+     * 进程成功执行信息
+     */
+    private String message;
+
+    /**
+     * 进程失败执行信息
+     */
+    private String errorMessage;
+
+    /**
+     * 进程执行耗时
+     */
+    private Long time;
+
+}
